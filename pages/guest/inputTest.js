323fb15b60651c1359b1af1b7f82bee1864bdc76
aion0721 <rudolf.aion@gmail.com>
2021-01-17 17:07:32 +0900

    Complete create item

--- pages/guest/inputTest.js
+++ pages/guest/inputTest.js
@@ -3,6 +3,7 @@ import { useForm, Controller } from "react-hook-form";
 import {
   FormLabel,
   RadioGroup,
+  FormControl,
   FormControlLabel,
   Radio,
   Paper,
@@ -10,9 +11,17 @@ import {
   Button,
   TextField,
 } from "@material-ui/core";
+import axios from "axios";
 
 function Top() {
-  const { control, handleSubmit, register, watch, getValues } = useForm();
+  const {
+    control,
+    handleSubmit,
+    register,
+    watch,
+    getValues,
+    setValue,
+  } = useForm();
   const watchAllFields = watch();
   const onSubmit = (register) => console.log(register);
 
@@ -25,12 +34,35 @@ function Top() {
   function confirmValue() {}
 
   function getAddress() {
+    const zipNumTmp = getValues("zipNum");
     console.log(getValues("zipNum"));
+    axios
+      .get(
+        "https://madefor.github.io/postal-code-api/api/v1/" +
+          zipNumTmp.substr(0, 3) +
+          "/" +
+          zipNumTmp.substr(3, 4) +
+          ".json"
+      )
+      .then((results) => {
+        const data = results.data.data[0].ja;
+        console.log(data);
+        const concatAddress =
+          data.prefecture +
+          data.address1 +
+          data.address2 +
+          data.address3 +
+          data.address4;
+        setValue("address1", concatAddress, {
+          shouldValidate: true,
+          shouldDirty: true,
+        });
+      });
   }
 
   return (
     <div>
-      <div style={{ visibility: confirmFlag ? "hidden" : "" }}>
+      <div style={{ display: !confirmFlag ? "block" : "none" }}>
         <form onSubmit={handleSubmit(onSubmit)}>
           <Paper>
             <TextField
@@ -66,27 +98,28 @@ function Top() {
               name="firstNameEng"
             ></TextField>
           </Paper>
-          <br />
-          <Divider />
+
           <Paper>
-            <FormLabel component="legend">性別</FormLabel>
-            <RadioGroup aria-label="gender" name="gender">
-              <FormControlLabel
-                value="female"
-                control={<Radio inputRef={register} />}
-                label="Female"
-              />
-              <FormControlLabel
-                value="male"
-                control={<Radio inputRef={register} />}
-                label="male"
-              />
-            </RadioGroup>
+            <FormControl component="fieldset">
+              <FormLabel component="legend">性別</FormLabel>
+              <RadioGroup aria-label="gender" name="gender">
+                <FormControlLabel
+                  value="female"
+                  control={<Radio inputRef={register} />}
+                  label="Female"
+                />
+                <FormControlLabel
+                  value="male"
+                  control={<Radio inputRef={register} />}
+                  label="male"
+                />
+              </RadioGroup>
+            </FormControl>
           </Paper>
           <Paper>
             <TextField
               inputRef={register}
-              label="住所"
+              label="郵便番号"
               name="zipNum"
             ></TextField>
             <Button
@@ -96,6 +129,31 @@ function Top() {
             >
               GETADDRESS
             </Button>
+            <Controller
+              name="address1"
+              control={control}
+              defaultValue=""
+              render={({ onChange, value }) => (
+                <TextField label="住所1" onChange={onChange} value={value} />
+              )}
+            />
+            <TextField
+              inputRef={register}
+              label="住所2"
+              name="address2"
+            ></TextField>
+          </Paper>
+          <Paper>
+            <TextField
+              inputRef={register}
+              label="電話番号"
+              name="phoneNumber"
+            ></TextField>
+            <TextField
+              inputRef={register}
+              label="メールアドレス"
+              name="mailaddress"
+            ></TextField>
           </Paper>
           <Button
             variant="contained"
@@ -106,12 +164,13 @@ function Top() {
           </Button>
         </form>
       </div>
-      <div style={{ visibility: !confirmFlag ? "hidden" : "" }}>
+      <div style={{ display: confirmFlag ? "block" : "none" }}>
         <form>
           <h1>Confirm</h1>
           {watchAllFields.lastNameJap}
           {watchAllFields.firstNameJap}
           {watchAllFields.gender}
+          {watchAllFields.address1}
           <Button type="submit" variant="contained" color="primary">
             Submit!
           </Button>
